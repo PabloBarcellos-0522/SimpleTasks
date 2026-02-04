8c319dd72e85451a660976ceb2070dbd89dd30d2
Pablo Barcellos Soares <pablobarcellossoares@gmail.com>
2026-02-04 17:04:15 -0300

    🚀 Configure API for production.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://simpletasks-j6vk.onrender.com",
 })
 
 export const getTarefas = async () => {
